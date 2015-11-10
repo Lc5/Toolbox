dc1655abb4e51df9e496f6ab267abe8ae93ddc47
Lukasz Krzyszczak <lkrzyszczak@divante.pl>
2015-11-10 20:40:58 +0100

    Convert some old left-over tabs to spaces

--- src/Timer.php
+++ src/Timer.php
@@ -26,41 +26,41 @@ class Timer
     private $isStarted;
 
     public function __construct()
-	{
-		$this->reset();
-	}
+    {
+        $this->reset();
+    }
 
     public function reset()
-	{
-		$this->startTime = 0.0;
-		$this->stopTime  = 0.0;
-		$this->isStarted = false;
-	}
+    {
+        $this->startTime = 0.0;
+        $this->stopTime  = 0.0;
+        $this->isStarted = false;
+    }
 
     public function start()
-	{
-		if (!$this->isStarted) {
-			$this->reset();
-			$this->startTime = microtime(true);
-			$this->isStarted = true;
-		}
-	}
+    {
+        if (!$this->isStarted) {
+            $this->reset();
+            $this->startTime = microtime(true);
+            $this->isStarted = true;
+        }
+    }
 
     public function stop()
-	{
-		if ($this->isStarted) {
-			$this->stopTime  = microtime(true);
-			$this->isStarted = false;
-		}
-	}
+    {
+        if ($this->isStarted) {
+            $this->stopTime  = microtime(true);
+            $this->isStarted = false;
+        }
+    }
 
     /**
      * @return float
      */
     public function getTime()
-	{
-		$stopTime = $this->isStarted ? microtime(true) : $this->stopTime;
+    {
+        $stopTime = $this->isStarted ? microtime(true) : $this->stopTime;
 
-		return $stopTime - $this->startTime;
-	}
+        return $stopTime - $this->startTime;
+    }
 }
