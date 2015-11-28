f246225a8a15b5c5057244aa08f8ff4834adfa99
Lukasz Krzyszczak <lkrzyszczak@divante.pl>
2015-11-28 21:44:04 +0100

    Add test for TraceableSoapClient

---
+++ tests/LoggingSoapClient/TraceableSoapClientTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lc5\Toolbox\LoggingSoapClient;
+
+/**
+ * Class TraceableSoapClientTest
+ *
+ * @author £ukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
+ */
+class TraceableSoapClientTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstruct()
+    {
+        $client     = new TraceableSoapClient(null, ['uri' => 'http://example.com', 'location' => 'http://example.com']);
+        $properties = get_object_vars($client);
+
+        $this->assertInstanceOf('Lc5\Toolbox\LoggingSoapClient\TraceableSoapClient', $client);
+        $this->assertArrayHasKey('trace', $properties);
+        $this->assertEquals(1, $properties['trace']);
+    }
+}
